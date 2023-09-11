e322083f32f00120ffa399cc217134f3544bfcbb
Duong Thang <duongthang0702@gmail.com>
2023-09-12 01:18:23 +0700

    Add scoped & config entity

--- src/Core/Db/BlogAppContext.cs
+++ src/Core/Db/BlogAppContext.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Core.Db.Entities;
+using Microsoft.EntityFrameworkCore;
 namespace Core.Db
 {
     public class BlogAppContext : DbContext
     {
-        public BlogAppContext(DbContextOptions options) : base(options)
+        public BlogAppContext(DbContextOptions<BlogAppContext> options) : base(options)
         {
 
+        }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
         }
     }
 }
--- src/Core/Db/Entities/UserEntiry.cs
+++ src/Core/Db/Entities/UserEntiry.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 namespace Core.Db.Entities
 {
-    public class UserEntiry : BaseEntity
+    public class UserEntity : BaseEntity
     {
         public string LastName { get; set; } = default!;
         public string FirstName { get; set; } = default!;
         public string Email { get; set; } = default!;
         public string Password { get; set; } = default!;
         public string Avatar { get; set; } = default!;
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<UserEntity>
+    {
+        public void Configure(EntityTypeBuilder<UserEntity> builder)
+        {
+            builder.HasIndex(x => x.Email).IsUnique();
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(30);
+            builder.Property(x => x.LastName).HasMaxLength(30);
+            builder.Property(x => x.FirstName).HasMaxLength(30);
+            builder.Property(x => x.Password).HasMaxLength(16).IsRequired();
+        }
+
     }
 }
