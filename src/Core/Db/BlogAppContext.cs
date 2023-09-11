0eb427ac146f776f8e6ca08b15b3adebc5d2a3ca
Duong Thang <duongthang0702@gmail.com>
2023-09-12 00:47:11 +0700

    initial

---
+++ src/Core/Db/BlogAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Db
+{
+    public class BlogAppContext : DbContext
+    {
+        public BlogAppContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ src/Core/Db/Entities/UserEntiry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Db.Entities
+{
+    public class UserEntiry : BaseEntity
+    {
+        public string LastName { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Avatar { get; set; } = default!;
+    }
+}
